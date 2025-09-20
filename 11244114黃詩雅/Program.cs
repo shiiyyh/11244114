9a1a68a964bd8cef91d34c2510f1f4a7a63683d9
shiiyyh <angel9312030@gmail.com>
2025-09-20 23:20:50 +0800

    2

--- 11244114黃詩雅/Program.cs
+++ 11244114黃詩雅/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("11244114 黃詩雅");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 黃詩雅11244114
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("11244114 黃詩雅");
+            Console.ReadLine();
+        }
+    }
+}
